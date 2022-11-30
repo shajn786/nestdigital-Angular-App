ff67706275dce7655b726b9565689eaeff0c8b4c
shajn786 <shajn786@gmail.com>
2022-11-30 14:25:28 +0530

    login,addcourse,addfriend

---
+++ src/app/employee-add-friend/employee-add-friend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-add-friend',
+  templateUrl: './employee-add-friend.component.html',
+  styleUrls: ['./employee-add-friend.component.css']
+})
+export class EmployeeAddFriendComponent {
+
+}
